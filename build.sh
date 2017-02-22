4d3fd6ebcebfd028baf9d310e577d5ed408dcf24
Ilia Kravtsov <idakraftsoft@gmail.com>
2017-02-23 01:34:28 +0300

    build package

--- build.sh
+++ build.sh
@@ -14,3 +14,5 @@ mkdir PRJ_ROOT
 rsync -av --progress ../.meta/ "$PWD"
 rsync -av --progress ../* $PRJ_ROOT --exclude=.git/ --exclude=.idea/ --exclude='*.pyc' --exclude='*.pyo' --exclude='__pycache__' --exclude=uploads/ --exclude=.meta/
 
+fakeroot dpkg-deb --build "$PWD" /tmp   
+
