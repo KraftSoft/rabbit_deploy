8816ee9d09548ac517e8abf0e653d1211a6195ea
Ilia Kravtsov <idakraftsoft@gmail.com>
2017-02-23 01:59:54 +0300

    fix path

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ fi
 
 PRJ_ROOT="usr/share/${PROJECT}-${VERSION}"
 
-mkdir PRJ_ROOT
+mkdir -p PRJ_ROOT
 
 rsync -av --progress ../.meta/ "$PWD"
 rsync -av --progress ../* $PRJ_ROOT --exclude=.git/ --exclude=.idea/ --exclude='*.pyc' --exclude='*.pyo' --exclude='__pycache__' --exclude=uploads/ --exclude=.meta/
 
-fakeroot dpkg-deb --build "$PWD" /tmp
+fakeroot dpkg-deb --build "$PWD/main" /tmp
 
