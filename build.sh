c0527ff6173ba7e5c602d4b433052120fa26cdc7
kic <kravcov2512@gmail.com>
2017-03-26 13:07:29 +0300

    update

--- build.sh
+++ build.sh
@@ -11,8 +11,9 @@ PRJ_ROOT="main/usr/share/${PROJECT}-${VERSION}"
 
 mkdir -p $PRJ_ROOT
 
-rsync -av --progress ../.meta/ "$PWD/main"
-rsync -av --progress ../* $PRJ_ROOT --exclude=.git/ --exclude=.idea/ --exclude='*.pyc' --exclude='*.pyo' --exclude='__pycache__' --exclude=uploads/ --exclude=.meta/ --exclude=deploy_tools
+rsync -av ../.meta/ "$PWD/main"
+rsync -av ../* $PRJ_ROOT --exclude=.git/ --exclude=.idea/ --exclude='*.pyc' --exclude='*.pyo' --exclude='__pycache__' --exclude=uploads/ --exclude=.meta/ --exclude=deploy_tools --exclude 'env*' --exclude 'core/migrations/*' --exclude 'chat/migrations/*' --exclude 'location/migrations/*' --exclude 'tags/migrations/*'
+
+rsync -av ~/env $PRJ_ROOT
 
 fakeroot dpkg-deb --build "$PWD/main" /tmp/${PROJECT}-${VERSION}.deb
-
