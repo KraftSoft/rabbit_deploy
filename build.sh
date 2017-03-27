279e4a093f11ea510ceba6fdbc965a9d6c1f6849
Ilia Kravtsov <idakraftsoft@gmail.com>
2017-03-28 00:19:03 +0300

    change env

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ mkdir -p $PRJ_ROOT
 rsync -av ../.meta/ "$PWD/main"
 rsync -av ../* $PRJ_ROOT --exclude=.git/ --exclude=.idea/ --exclude='*.pyc' --exclude='*.pyo' --exclude='__pycache__' --exclude=uploads/ --exclude=.meta/ --exclude=deploy_tools --exclude 'env*' --exclude 'core/migrations/*' --exclude 'chat/migrations/*' --exclude 'location/migrations/*' --exclude 'tags/migrations/*'
 
-rsync -av ~/env $PRJ_ROOT
+rsync -av ../env $PRJ_ROOT
 
 fakeroot dpkg-deb --build "$PWD/main" /tmp/${PROJECT}-${VERSION}.deb
